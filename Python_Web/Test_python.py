81ce21d0caf12e97279cdba081b4927b3ddebc4d
yanyanyan <ymh007001@163.com>
2022-05-24 01:06:46 +0800

    master

--- Python_Web/Test_python.py
+++ Python_Web/Test_python.py
@@ -1,12 +1,8 @@
-print("hellor")
-a = 5
-b = 6
-print(a+b)
-print("fgggg")
-print("fgggg")
-print("fgggg")
-print("fgggg")
-print("fgggg")
-print("fgggg")
-print("fgggg")
-print("fgggg")
+for i in range(1, 10):
+    for j in range(1, 10):
+        if j <= i:
+            sum = i * j
+            print("{}*{}={}\t".format(i, j, sum), end=" ")
+        j += 1
+    i += 1
+    print(" ")
